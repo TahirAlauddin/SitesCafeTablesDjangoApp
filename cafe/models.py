eac6fccbb89e38907475487d3c3d452a36196204
Tahir <tahiralauddin7@gmail.com>
2022-05-22 00:07:54 +0500

    cafe_table column id max-length changed from 10 to 50

--- cafe/models.py
+++ cafe/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Table(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
+    id = models.CharField(max_length=50, primary_key=True)
     top = models.IntegerField(default=0)
     left = models.IntegerField(default=0)
     label = models.CharField(max_length=50)
